32cf97ef7d2eca9510a2a074bdf5080d7a21f09f
Eduardo Fontenele <eduardofontedev@gmail.com>
2024-09-28 21:52:51 -0300

    edfv

--- src/main/java/com/oldSpringBatch/controller/JobController.java
+++ src/main/java/com/oldSpringBatch/controller/JobController.java
@@ -3,6 +3,7 @@ package com.oldSpringBatch.controller;
 import com.oldSpringBatch.service.JobRunner;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobController {
 
     private final JobRunner jobRunner;
+    private final JobOperator jobOperator;
 
     @GetMapping("/{name}")
     public String startJob(@PathVariable String name) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
@@ -24,6 +26,7 @@ public class JobController {
         if (name != null) {
             jobRunner.runJob(name);
         }
+
 
         return String.format("Processing... Wait til is done. Job name: %s", name);
     }
